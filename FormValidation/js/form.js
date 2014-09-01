a46efa8977c9170f72f85f984785dba943b3e301
moutusi <mbiswas646@gmail.com>
2014-09-01 15:58:49 -0500

    Validation message added

--- FormValidation/js/form.js
+++ FormValidation/js/form.js
@@ -3,14 +3,22 @@
  */
 //'use strict';
 someapp.controller('myformController',['$scope',function($scope){
-    $scope.something = "hi";
-    $scope.a = "";
+
+    $scope.a = {};
     $scope.reset= function(){
-        $scope.user = "";
-        $scope.a = "";
+//        $scope.user = "";
+//        $scope.a = "";
+        $scope.user = angular.copy($scope.a);
+
     };
     $scope.save= function(user){
         $scope.a = angular.copy(user);
     };
+
+    $scope.isUnchanged = function(user){
+        return angular.equals(user,$scope.a );
+    }
+
+    $scope.reset();
 
 }]);
